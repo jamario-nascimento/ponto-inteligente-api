65c05acdc703f24c94f12a9cb1f3131f06e068bb
Jamário Nascimento <jamariobatista@gmail.com>
2023-12-30 13:49:31 -0300

    ajustes segirança e refatoramento

---
+++ src/main/java/com/jamario/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.jamario.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
--- src/main/java/com/kazale/pontointeligente/api/enums/PerfilEnum.java
+++
@@ -1,6 +0,0 @@
-package com.kazale.pontointeligente.api.enums;
-
-public enum PerfilEnum {
-	ROLE_ADMIN,
-	ROLE_USUARIO;
-}
